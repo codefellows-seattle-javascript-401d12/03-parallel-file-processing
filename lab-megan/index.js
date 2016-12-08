ffbd4e43e40145fcf2bfbe82f586cd5ccadb2a48
Megan Reardon <meganr@gmail.com>
2016-12-08 10:25:49 -0800

    index and files-reader files structure half built

--- lab-megan/index.js
+++ lab-megan/index.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const reader = require('./lib/files-reader.js');
+
+reader('one.txt', function(err, data) {
+  if (err) throw err;
+  console.log(data);
+});
+
+
+
+// code below brough in from day 2 lab
+
+// const greet = require('./lib/greetings.js');
+//
+// process.argv.forEach((val, index) => {
+//   console.log(`${index}: ${val}`);
+// });
+//
+// greet.sayHello('Scott');
+// greet.sayGoodbye();
--- lab-megan/lib/files-reader.js
+++ lab-megan/lib/files-reader.js
@@ -3,11 +3,18 @@
 const fs = require('fs');
 
 // the code below is copied from the demo files, don't worry I typed it out
-const fileReader = module.exports = function(file, callback) {
-  fs.readFile(file, function(err, data) {
+const fileReader = module.exports = function(fileName, callback) {
+  // var theFiles = [];
+  fs.readFile(`${__dirname}/../data/${fileName}`, function(err, data) {
     if (err) return callback(err);
     // console.log('can you see me?');
-    // return callback(null, data.toString());
     return callback(null, data.toString('hex', 0, 8));
+    // get rid of return above, will exit function
   });
 };
+
+// kaylyn notes:
+// use array
+// nested madness goes here
+
+// will be invoking this file in index.js
