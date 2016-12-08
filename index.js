c418bd02ad3732f28809188cacc8a94818ca6dd0
Caleb Sattgast <calebsattgast@gmail.com>
2016-12-08 08:36:16 -0800

    successfully write script to truncate txt file after first 8 bytes

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
 'use strict';
 
-// awesome code goes here
 const fs = require('fs');
 
-fs.readFile('data/september.txt', function(err, data) {
-  if (err) throw err;
-  console.log('Poem output:', data.toString());
-});
-
-fs.stat('data/september.txt', function(err, stats) {
-  if (err) throw err;
-  console.log(stats);
-  console.log('File info successfully fetched');
-});
+// Open file, truncate data to first 8 bytes, print result in hex form
 
 fs.open('data/september.txt', 'r+', function(err, fileDesc) {
   if (err) throw err;
@@ -21,5 +11,10 @@ fs.open('data/september.txt', 'r+', function(err, fileDesc) {
   // truncate after 8 bytes
   fs.ftruncate(fileDesc, 8, function(err) {
     if (err) throw err;
+  });
+
+  fs.readFile('data/september.txt', function(err, data) {
+    if (err) throw err;
+    console.log('Truncated poem output', data.toString('hex'));
   });
 });
