1b06fba41fe03ef9226955e7b716cbfaa31aebbe
JINHO PAIK <JMPAIK83@GMAIL.COM>
2016-12-07 20:38:43 -0800

    trying to read file in order

--- lab-jinho/index.js
+++ lab-jinho/index.js
@@ -2,17 +2,26 @@
 
 const fs = require('fs');
 
-fs.readFile(`${__dirname}/data/one.txt`, function(err, data) {
+fs.readdir('./data', function(err, files) {
   if (err) throw err;
-  console.log('content:', data.toString('hex', 0, 8));
-});
+  console.log(files);
+  // var count = files.length,
+  // var results = {};
+  // files.forEach(function (filename) {
 
-fs.readFile(`${__dirname}/data/two.txt`, function(err, data) {
-  if (err) throw err;
-  console.log('content:', data.toString('hex', 0, 8));
-});
+    fs.readFile(`${__dirname}/data/one.txt`, function(err, data) {
+      if (err) throw err;
+      console.log('content:', data.toString('hex', 0, 8));
+    });
 
-fs.readFile(`${__dirname}/data/three.txt`, function(err, data) {
-  if (err) throw err;
-  console.log('content:', data.toString('hex', 0, 8));
+    fs.readFile(`${__dirname}/data/two.txt`, function(err, data) {
+      if (err) throw err;
+      console.log('content:', data.toString('hex', 0, 8));
+    });
+
+    fs.readFile(`${__dirname}/data/three.txt`, function(err, data) {
+      if (err) throw err;
+      console.log('content:', data.toString('hex', 0, 8));
+    });
+  // });
 });
