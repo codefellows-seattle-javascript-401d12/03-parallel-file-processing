4b66a683dab442e65edd01f0f3e6eaeb889e364c
kcirekcom <erick.f.mock@gmail.com>
2016-12-07 20:24:17 -0800

    nested callbacks to print in order

--- index.js
+++ index.js
@@ -4,15 +4,13 @@ const fs = require('fs');
 
 fs.readFile(`${__dirname}/data/one.txt`, function(err, data) {
   if (err) throw err;
-  console.log('content of the original file is:', data.toString('hex'));
-});
-
-fs.readFile(`${__dirname}/data/two.txt`, function(err, data) {
-  if (err) throw err;
-  console.log('content of the original file is:', data.toString('hex'));
-});
-
-fs.readFile(`${__dirname}/data/three.txt`, function(err, data) {
-  if (err) throw err;
-  console.log('content of the original file is:', data.toString('hex'));
+  console.log('content of the first file is:', data.toString('hex', 0, 8));
+  fs.readFile(`${__dirname}/data/two.txt`, function(err, data) {
+    if (err) throw err;
+    console.log('content of the second file is:', data.toString('hex', 0, 8));
+    fs.readFile(`${__dirname}/data/three.txt`, function(err, data) {
+      if (err) throw err;
+      console.log('content of the third file is:', data.toString('hex', 0, 8));
+    });
+  });
 });
