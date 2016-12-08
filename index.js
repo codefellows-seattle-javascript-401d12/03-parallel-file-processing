a2178f1a049abf7dbf88db5e187a28e2cce73328
Peter Kim <peterkim23@gmail.com>
2016-12-07 19:20:43 -0800

    read file is printing all three data files. created read-file-test.js file.

--- index.js
+++ index.js
@@ -3,19 +3,4 @@
 const fs = require('fs');
 const readFile = require('./lib/read-file.js');
 
-// fs.readFile(`${__dirname}/data/one.txt`, function(err, data) {
-//   if (err) throw err;
-//   console.log(data.toString('hex', 0, 8));
-// });
-//
-// fs.readFile(`${__dirname}/data/two.txt`, function(err, data) {
-//   if (err) throw err;
-//   console.log(data.toString());
-// });
-//
-// fs.readFile(`${__dirname}/data/three.txt`, function(err, data) {
-//   if (err) throw err;
-//   console.log(data.toString());
-// });
-
-readFile.readFirst();
+readFile.fileReader();
