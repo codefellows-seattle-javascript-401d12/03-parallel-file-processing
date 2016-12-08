a5ca10a720685e076aa18805d8b950b4faf2cc1c
Daniel Becker <dbecker4130@gmail.com>
2016-12-08 12:05:22 -0800

    proper order test passes

--- test/hex-test.js
+++ test/hex-test.js
@@ -9,9 +9,27 @@ describe('Read File', function() {
       readFile(`${__dirname}/not-a-file.txt`, function(err) {
         expect(err).to.be.an('error');
         done();
-
       });
     });
-
+  });
+  describe('#readFile', function() {
+    it('should display in proper order', function(done) {
+      readFile(`${__dirname}/../data/one.txt`, function(err, data) {
+        expect(err).to.equal(null);
+        expect(data).to.be.a('string');
+        expect(data).to.equal('276f6e6520617765');
+        readFile(`${__dirname}/../data/two.txt`, function(err, data) {
+          expect(err).to.equal(null);
+          expect(data).to.be.a('string');
+          expect(data).to.equal('277365636f6e6420');
+          readFile(`${__dirname}/../data/three.txt`, function(err, data) {
+            expect(err).to.equal(null);
+            expect(data).to.be.a('string');
+            expect(data).to.equal('2774686972642073');
+            done();
+          });
+        });
+      });
+    });
   });
 });
