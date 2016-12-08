0c7fecc2de7f544c3e03ff1fa636259c5456a484
Megan Reardon <meganr@gmail.com>
2016-12-07 17:36:11 -0800

    filled in data files, lib and test files

--- lab-megan/lib/files-reader.js
+++ lab-megan/lib/files-reader.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const fs = require('fs');
+
+// the code below is copied from the demo files, don't worry I typed it out
+const fileReader = module.exports = function(file, callback) {
+  fs.readFile(file, function(err, data) {
+    if (err) return callback(err);
+    return callback(null, data.toString());
+  });
+};
--- lab-megan/test/files-reader-test.js
+++ lab-megan/test/files-reader-test.js
@@ -1,1 +1,28 @@
 'use strict';
+
+// the code below was copied from the demo code from class, don't worry I typed it all out
+
+const expect = require('chai').expect;
+const fileReader = require('../lib/files-reader.js');
+
+describe('File Reader Module', function() {
+  describe('with an improper file path', function() {
+    it('should return an error', function(done) {
+      fileReader(`${__dirname}/not-a-file.txt`, function(err) {
+        expect(err).to.be.an('error');
+        done();
+      });
+    });
+  });
+
+  describe('with a proper file path', function() {
+    it('should return the contents of the file', function(done) {
+      fileReader(`${__dirname}/../data/data.txt`, function(err, data) {
+        expect(err).to.equal(null);
+        expect(data).to.be.a('string');
+        expect(data).to.equal('sample data to be read from the read file demo\n');
+        done();
+      });
+    });
+  });
+});
