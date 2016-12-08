d007be5dc4e4a7ee87ab1449939a06d27b1587e7
Geoff Simons <geoff.simons@gmail.com>
2016-12-07 20:58:03 -0800

    Testing main console output of index.js
    I used child_process.exec to run our index.js,
    which is expected to write the first 8 bytes
    of 3 specified files as hex. To compute the
    expected values, I used:
    xxd -p filename | head -c 16
    (8 Bytes -> 16 chars in hex)

---
+++ lab-geoff/test/index-test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const exec   = require('child_process').exec;
+const expect = require('chai').expect;
+
+describe('Index: index.js', function() {
+  describe('Main execution', function() {
+    it('should log the first 8 bytes of the files in order', function(done) {
+      // I used the bash command: xxd -p filename | head -c 16
+      // to determine the expected results from a known
+      // utility.
+      const expectedResult = [
+        '6f6e652e74787420',
+        '74776f2e74787420',
+        '74687265652e7478'
+      ].join('\n').trim();
+
+      // exec('node index.js', function(err, stdout, stderr) {
+      exec('node index.js', function(err, stdout) {
+        expect(err).to.not.be.an('error');
+        expect(stdout.trim()).to.equal(expectedResult);
+        done();
+      });
+    });
+  });
+});
