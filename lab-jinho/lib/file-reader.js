ceade984a26746266cdfbe82c5de9de9df505923
JINHO PAIK <JMPAIK83@GMAIL.COM>
2016-12-08 14:23:20 -0800

    add read file test

--- lab-jinho/lib/file-reader.js
+++ lab-jinho/lib/file-reader.js
@@ -2,9 +2,20 @@
 
 const fs = require('fs');
 
-const fileReader = module.exports = function(file, callback){
-  fs.readFile(file, function(err, data){
+const fileReader = module.exports = function(pathArray, callback){
+  var result = [];
+  fs.readFile(pathArray[0], function(err, data){
     if (err) return callback(err);
-    return callback(null, data.toString());
+    result.push(data.toString('hex', 0, 8));
+    fs.readFile(pathArray[1], function(err, data) {
+      if (err) return callback(err);
+      result.push(data.toString('hex', 0, 8));
+      fs.readFile(pathArray[2], function(err, data) {
+        if (err) return callback(err);
+        result.push(data.toString('hex', 0, 8));
+        console.log('result:', result);
+        callback(null, result);
+      });
+    });
   });
 };
--- lab-jinho/test/read-file-test.js
+++ lab-jinho/test/read-file-test.js
@@ -3,22 +3,31 @@
 const expect = require('chai').expect;
 const fileReader = require('../lib/file-reader.js');
 
-describe('File Reader Module', function() {
-  describe('with an improper file path', function() {
-    it('should return an error', function(done) {
-      fileReader(`${__dirname}/not-a-file.txt`, function(err) {
-        expect(err).to.be.an('error');
-        done();
+describe('testing File Reader Module', function() {
+  describe('with bad file paths', function() {
+    it('should return an error', function() {
+      fileReader([`${__dirname}/no-file-exists.txt`], function(err) {
+        expect(err).to.equal(true);
       });
     });
   });
 
-  describe('with a proper file path', function() {
-    it('should return the contents of the file', function(done) {
-      fileReader(`${__dirname}/../data/data.txt`, function(err, data) {
+  describe('with proper file paths', function() {
+    before((done) => {
+      this.paths = [
+        `${__dirname}/../data/one.txt`,
+        `${__dirname}/../data/two.txt`,
+        `${__dirname}/../data/three.txt`
+      ];
+      done();
+    });
+    it('should have correct string order', (done) => {
+      var expectedResult = [];
+      fileReader(this.paths, function(err, data) {
         expect(err).to.equal(null);
-        expect(data).to.be.a('string');
-        expect(data).to.equal('sample data to be read from the read file demo\n');
+        expect(data[0]).to.equal(expectedResult[0]);
+        expect(data[1]).to.equal(expectedResult[1]);
+        expect(data[2]).to.equal(expectedResult[2]);
         done();
       });
     });
