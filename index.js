1fa5de131516a0fed14c9103a76b5633cc713478
kcirekcom <erick.f.mock@gmail.com>
2016-12-08 12:03:16 -0800

    converted back to log 8 bytes of hex

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const fileReader = require('./lib/file-reader.js');
 
 fileReader(`${__dirname}/data/one.txt`, function(err, data) {
   if (err) throw err;
-  console.log('content of the first file is:', data);
+  console.log('content of the first file is:', data.toString('hex', 0, 8));
   fileReader(`${__dirname}/data/two.txt`, function(err, data) {
     if (err) throw err;
-    console.log('content of the second file is:', data);
+    console.log('content of the second file is:', data.toString('hex', 0, 8));
     fileReader(`${__dirname}/data/three.txt`, function(err, data) {
       if (err) throw err;
-      console.log('content of the third file is:', data);
+      console.log('content of the third file is:', data.toString('hex', 0, 8));
     });
   });
 });
