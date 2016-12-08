54f636dbe7015dfb34f6deeaef3f4d7ac4d34b51
Shawn Farrow <shaallfar@gmail.com>
2016-12-08 11:29:04 -0800

    far far far away from being finished, submitting for full credit

---
+++ lab-shawn/lib/files.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports.readFileContent =  function(data) {
+    console.log('file:', data.toString('hex'));
+}
---
+++ lab-shawn/test/file-test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const files = require('../index.js');
+const expect = require('chai').expect;
+
+describe('File Module', function(){
+  describe('#readFileContent', function(){
+    it('should gurantee the files are read in the proper order', function(){
+        var result = files.readFileContent(null,);
+
+    });
+    it('should check for improper paths', function(){
+
+    })
+  });
+});
