98a8212dfec0d402c848e3ac31ef87d3567d62a4
JINHO PAIK <JMPAIK83@GMAIL.COM>
2016-12-08 09:09:23 -0800

    files read in order after nesting

--- lab-jinho/index.js
+++ lab-jinho/index.js
@@ -2,26 +2,17 @@
 
 const fs = require('fs');
 
-fs.readdir('./data', function(err, files) {
+fs.readFile(`${__dirname}/data/one.txt`, function(err, data) {
   if (err) throw err;
-  console.log(files);
-  // var count = files.length,
-  // var results = {};
-  // files.forEach(function (filename) {
+  console.log('content:', data.toString('hex', 0, 8));
 
-    fs.readFile(`${__dirname}/data/one.txt`, function(err, data) {
-      if (err) throw err;
-      console.log('content:', data.toString('hex', 0, 8));
-    });
-
-    fs.readFile(`${__dirname}/data/two.txt`, function(err, data) {
-      if (err) throw err;
-      console.log('content:', data.toString('hex', 0, 8));
-    });
+  fs.readFile(`${__dirname}/data/two.txt`, function(err, data) {
+    if (err) throw err;
+    console.log('content:', data.toString('hex', 0, 8));
 
     fs.readFile(`${__dirname}/data/three.txt`, function(err, data) {
       if (err) throw err;
       console.log('content:', data.toString('hex', 0, 8));
     });
-  // });
+  });
 });
