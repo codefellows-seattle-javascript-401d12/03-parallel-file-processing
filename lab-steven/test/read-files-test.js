95ae4d70fa8d8969ee9d7431152e9cf206194cfa
Steven Bateman <batemanvo@gmail.com>
2016-12-08 08:17:48 -0800

    Updated test. Now the test to make sure the array of hex values is in proper order checks the result of performing a readFile on one.txt, two.txt, and three.txt, so that if something changes in the files, the test does not have to be updated to new literal values.

--- lab-steven/test/read-files-test.js
+++ lab-steven/test/read-files-test.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const fs = require('fs');
 const expect = require('chai').expect;
 const fileReader = require('../lib/read-files.js');
 
@@ -13,9 +14,18 @@ describe('File reader', function() {
 
   it('should return an array of the first 8 hex values of the buffers from one.txt, two.txt, and three.txt, in that order', function() {
     fileReader([`${__dirname}/data/one.txt`, `${__dirname}/data/two.txt`, `${__dirname}/data/three.txt`], function(err, data) {
-      expect(data[0]).to.equal('46696c65206f6e65');
-      expect(data[1]).to.equal('46696c652074776f');
-      expect(data[2]).to.equal('46696c6520746872');
+      const resultOne = fs.readFile(`${__dirname}/../data/one.txt`, function(err, data) {
+        return data.toString('hex', 0, 8);
+      });
+      const resultTwo = fs.readFile(`${__dirname}/../data/two.txt`, function(err, data) {
+        return data.toString('hex', 0, 8);
+      });
+      const resultThree = fs.readFile(`${__dirname}/../data/three.txt`, function(err, data) {
+        return data.toString('hex', 0, 8);
+      });
+      expect(data[0]).to.equal(resultOne);
+      expect(data[1]).to.equal(resultTwo);
+      expect(data[2]).to.equal(resultThree);
     });
   });
 });
