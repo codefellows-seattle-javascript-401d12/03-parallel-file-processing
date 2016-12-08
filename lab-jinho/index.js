9fc6f114a439a0bc57711d452fa888dbe39c37aa
JINHO PAIK <JMPAIK83@GMAIL.COM>
2016-12-07 17:29:27 -0800

    prints first 8

--- lab-jinho/index.js
+++ lab-jinho/index.js
@@ -2,17 +2,17 @@
 
 const fs = require('fs');
 
-fs.readFile(`${__dirname}/data/one.txt`, 'hex', function(err, data) {
+fs.readFile(`${__dirname}/data/one.txt`, function(err, data) {
   if (err) throw err;
-  console.log('content:', data.toString());
+  console.log('content:', data.toString('hex', 0, 8));
 });
 
-fs.readFile(`${__dirname}/data/two.txt`, 'hex', function(err, data) {
+fs.readFile(`${__dirname}/data/two.txt`, function(err, data) {
   if (err) throw err;
-  console.log('content:', data.toString());
+  console.log('content:', data.toString('hex', 0, 8));
 });
 
-fs.readFile(`${__dirname}/data/three.txt`, 'hex', function(err, data) {
+fs.readFile(`${__dirname}/data/three.txt`, function(err, data) {
   if (err) throw err;
-  console.log('content:', data.toString());
+  console.log('content:', data.toString('hex', 0, 8));
 });
