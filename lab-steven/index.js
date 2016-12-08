7430767137f451a357b75b25ee414e6d2a1f2023
Steven Bateman <batemanvo@gmail.com>
2016-12-07 20:21:50 -0800

    Rewrote read-files.js and index.js. Now uses nested fs.readFiles instead of fs.readFileSyncs. Returns an array of the first 8 hex values of the buffer file.

--- lab-steven/index.js
+++ lab-steven/index.js
@@ -1,3 +1,10 @@
 'use strict';
 
 const fileReader = require('./lib/read-files.js');
+
+fileReader([`${__dirname}/data/one.txt`, `${__dirname}/data/two.txt`, `${__dirname}/data/three.txt`], function(err, data) {
+  if (err) throw err;
+  data.forEach(function(file) {
+    console.log(file);
+  });
+});
--- lab-steven/lib/read-files.js
+++ lab-steven/lib/read-files.js
@@ -1,3 +1,20 @@
 'use strict';
 
 const fs = require('fs');
+
+const fileReader = module.exports = function(fileArray, callback) {
+  var arrayOfHexes = [];
+  fs.readFile(fileArray[0], function(err, data) {
+    if (err) return callback(err);
+    arrayOfHexes.push(data.toString('hex', 0, 8));
+    fs.readFile(fileArray[1], function(err, data) {
+      if (err) return callback(err);
+      arrayOfHexes.push(data.toString('hex', 0, 8));
+      fs.readFile(fileArray[2], function(err, data) {
+        if (err) return callback(err);
+        arrayOfHexes.push(data.toString('hex', 0, 8));
+        return callback(null, arrayOfHexes);
+      });
+    });
+  });
+};
