933a5bb9f0efcc091db10cb30263364284670f75
Caleb Sattgast <calebsattgast@gmail.com>
2016-12-08 10:00:29 -0800

    cleanup... remove unused variable

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 
 const truncate = require('./lib/truncate.js');
 
-var poemData = 'data/one.txt'
-
 var poemFiles = [`${__dirname}/data/one.txt`, `${__dirname}/data/two.txt`, `${__dirname}/data/three.txt`];
 
 poemFiles.forEach(
