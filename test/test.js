8edca4545de952b39c676818c63591b36155f408
Jonathan Heemstra <jonathan.heemstra@gmail.com>
2016-12-08 12:57:25 -0800

    broken :(

--- test/test.js
+++ test/test.js
@@ -2,7 +2,8 @@
 
 const expect = require('chai').expect;
 const fileReader = require('../lib/file-reader.js');
-let files = ['one.txt', 'two.txt', 'three.txt'];
+const index = require('../index.js');
+const files = require('../index.js').files;
 let i = 0;
 
 describe('Read File', function() {
@@ -11,29 +12,33 @@ describe('Read File', function() {
       return i++;
     });
     it(`should return the first 8 bytes of file ${files[0]}`, function(done) {
-      fileReader(`${__dirname}/../data/${files[i]}`, function(err, data) {
+      index.printFiles(`${__dirname}/../data/${files[i]}`, function(err, data) {
         console.log('file:', files[i]);
+        console.log('data = ', data);
         console.log('data', data.toString());
         expect(err).to.equal(null);
         expect(data).to.be.a('string');
+        expect(files[i]).to.equal(files[0]);
         done();
       });
     });
     it(`should return the first 8 bytes of file ${files[1]}`, function(done) {
-      fileReader(`${__dirname}/../data/${files[i]}`, function(err, data) {
+      index.printFiles(`${__dirname}/../data/${files[i]}`, function(err, data) {
         console.log('file:', files[i]);
         console.log('data', data.toString());
         expect(err).to.equal(null);
         expect(data).to.be.a('string');
+        expect(files[i]).to.equal(files[1]);
         done();
       });
     });
     it(`should return the first 8 bytes of file ${files[2]}`, function(done) {
-      fileReader(`${__dirname}/../data/${files[i]}`, function(err, data) {
+      index.printFiles(`${__dirname}/../data/${files[i]}`, function(err, data) {
         console.log('file:', files[i]);
         console.log('data', data.toString());
         expect(err).to.equal(null);
         expect(data).to.be.a('string');
+        expect(files[i]).to.equal(files[2]);
         done();
       });
     });
