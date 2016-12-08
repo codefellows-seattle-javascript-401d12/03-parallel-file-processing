485dd8a489fd0ecf7c69b0917f245f90e1dd0f21
Peter Kim <peterkim23@gmail.com>
2016-12-07 17:22:40 -0800

    created read-file module and buffer to print first 8 bytes

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 'use strict';
 
 const fs = require('fs');
+const readFile = require('./lib/read-file.js');
 
-fs.readFile(`${__dirname}/data/one.txt`, function(err, data) {
-  if (err) throw err;
-  console.log('one.txt string:', data.toString());
-});
+// fs.readFile(`${__dirname}/data/one.txt`, function(err, data) {
+//   if (err) throw err;
+//   console.log(data.toString('hex', 0, 8));
+// });
+//
+// fs.readFile(`${__dirname}/data/two.txt`, function(err, data) {
+//   if (err) throw err;
+//   console.log(data.toString());
+// });
+//
+// fs.readFile(`${__dirname}/data/three.txt`, function(err, data) {
+//   if (err) throw err;
+//   console.log(data.toString());
+// });
 
-fs.readFile(`${__dirname}/data/two.txt`, function(err, data) {
-  if (err) throw err;
-  console.log('two.txt string:', data.toString());
-});
-
-fs.readFile(`${__dirname}/data/three.txt`, function(err, data) {
-  if (err) throw err;
-  console.log('three.txt string:', data.toString());
-});
+readFile.readFirst();
