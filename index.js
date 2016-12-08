8cf292adaeaf065e854e8bc4493190b8b7042335
Caleb Sattgast <calebsattgast@gmail.com>
2016-12-08 09:01:09 -0800

    move truncate functionality to lib directory

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
 'use strict';
 
-const fs = require('fs');
-
-// Open file, truncate data to first 8 bytes, print result in hex form
-
-fs.open('data/september.txt', 'r+', function(err, fileDesc) {
-  if (err) throw err;
-  console.log('File opened.');
-
-  // truncate after 8 bytes
-  fs.ftruncate(fileDesc, 8, function(err) {
-    if (err) throw err;
-  });
-
-  fs.readFile('data/september.txt', function(err, data) {
-    if (err) throw err;
-    console.log('Truncated poem output', data.toString('hex'));
-  });
-});
+module.exports = exports = {};
