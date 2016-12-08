9854e7968b439f57b4c6e9f1c406fec56ce88e9e
Caleb Sattgast <calebsattgast@gmail.com>
2016-12-08 09:49:57 -0800

    add array of file paths

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const truncate = require('./lib/truncate.js');
 
 var poemData = 'data/one.txt'
 
-truncate.truncatePoem(poemData);
+var poemFiles = [`${__dirname}/data/one.txt`, `${__dirname}/data/two.txt`, `${__dirname}/data/three.txt`];
+
+truncate.truncatePoem(poemFiles[1]);
