ef3652e7e2947bd20e3faa65a82e1ed5b4d87337
Jonathan Heemstra <jonathan.heemstra@gmail.com>
2016-12-07 21:17:30 -0800

    run tests in order

--- test/test.js
+++ test/test.js
@@ -2,16 +2,40 @@
 
 const expect = require('chai').expect;
 const fileReader = require('../lib/file-reader.js');
-let files = [`${__dirname}/../data/one.txt`, `${__dirname}/../data/two.txt`, `${__dirname}/../data/three.txt`];
+let files = ['one.txt', 'two.txt', 'three.txt'];
+let i = 0;
 
 describe('Read File', function() {
   describe('proper file path', function() {
-    it('should return the contents of the file', function(done) {
-      fileReader(files[0], function(err, data) {
-        console.log('data', data.toString('utf-8'));
+    afterEach('add one to files iterator', function() {
+      return i++;
+    });
+    it(`should return the contents of file ${files[i]}`, function(done) {
+      fileReader(`${__dirname}/../data/${files[i]}`, function(err, data) {
+        console.log('file:', files[i]);
+        console.log('data', data.toString());
         expect(err).to.equal(null);
         done();
       });
     });
+    it(`should return the contents of file ${files[i]}`, function(done) {
+      fileReader(`${__dirname}/../data/${files[i]}`, function(err, data) {
+        console.log('file:', files[i]);
+        console.log('data', data.toString());
+        expect(err).to.equal(null);
+        done();
+      });
+    });
+    it(`should return the contents of file ${files[i]}`, function(done) {
+      fileReader(`${__dirname}/../data/${files[i]}`, function(err, data) {
+        console.log('file:', files[i]);
+        console.log('data', data.toString());
+        expect(err).to.equal(null);
+        done();
+      });
+    });
+  });
+  describe('', function(){
+
   });
 });
