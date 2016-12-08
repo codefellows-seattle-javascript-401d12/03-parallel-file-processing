65ec15900b52f95e1ce181ab930bb1e93d8807d0
Hawa Abdi <abdih17@hotmail.com>
2016-12-08 10:57:22 -0800

    All of my tests are passing.

--- test/file-reader-test.js
+++ test/file-reader-test.js
@@ -5,21 +5,21 @@ const expect = require('chai').expect;
 const fileReader = require('../lib/file-reader.js');
 
 describe('Reading all three files', function(){
-  describe('read files asynchronously?', function(){
-    // var threeFileData = [];
-    before( function(done){
-      fs.readFile(`${__dirname}/../data/one.txt`, function(err, data){
-        threeFileData.push(data.toString());
-        fs.readFile(`${__dirname}/../data/two.txt`, function(err, data){
-          threeFileData.push(data.toString());
-          fs.readFile(`${__dirname}/../data/three.txt`, function(err, data){
-            threeFileData.push(data.toString());
-            done();
-          });
-        });
-      });
-    });
-  });
+  // describe('read files asynchronously?', function(){
+  //   var threeFileData = [];
+  //   before( function(done){
+  //     fs.readFile(`${__dirname}/../data/one.txt`, function(err, data){
+  //       threeFileData.push(data.toString());
+  //       fs.readFile(`${__dirname}/../data/two.txt`, function(err, data){
+  //         threeFileData.push(data.toString());
+  //         fs.readFile(`${__dirname}/../data/three.txt`, function(err, data){
+  //           threeFileData.push(data.toString());
+  //           done();
+  //         });
+  //       });
+  //     });
+  //   });
+  // });
 
   it('should rad the files in order, and provide the first 8 bytes of each file', function(done){
     fileReader(`${__dirname}/../data/one.txt`, function(err, data){
