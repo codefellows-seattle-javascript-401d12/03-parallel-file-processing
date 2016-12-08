e5cbb31d61d53f41e5aada347ac02819b76faedf
Britt Dawn <brittdawndev@gmail.com>
2016-12-07 18:26:50 -0800

    Read three files and console log first 8 bytes in hex.

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const fs = require('fs');
+
+fs.readFile(`${__dirname}/data/one.txt`, function(err, data){
+  if (err) throw err;
+  console.log('1:', data.toString('hex', 0, 8));
+  fs.readFile(`${__dirname}/data/two.txt`, function(err, data){
+    if (err) throw err;
+    console.log('2:', data.toString('hex', 0, 8));
+    fs.readFile(`${__dirname}/data/three.txt`, function(err, data){
+      if (err) throw err;
+      console.log('3:', data.toString('hex', 0, 8));
+    });
+  });
+});
