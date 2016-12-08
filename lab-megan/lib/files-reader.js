dc6b798acec5e2235de1980e7af8d3cb45d08159
Megan Reardon <meganr@gmail.com>
2016-12-08 09:22:26 -0800

    basic test reading three txt files passing

--- lab-megan/lib/files-reader.js
+++ lab-megan/lib/files-reader.js
@@ -7,6 +7,7 @@ const fileReader = module.exports = function(file, callback) {
   fs.readFile(file, function(err, data) {
     if (err) return callback(err);
     // console.log('can you see me?');
-    return callback(null, data.toString());
+    // return callback(null, data.toString());
+    return callback(null, data.toString('hex', 0, 8));
   });
 };
--- lab-megan/test/files-reader-test.js
+++ lab-megan/test/files-reader-test.js
@@ -5,24 +5,76 @@
 const expect = require('chai').expect;
 const fileReader = require('../lib/files-reader.js');
 
-describe('File Reader Module', function() {
-  describe('with an improper file path', function() {
-    it('should return an error', function(done) {
-      fileReader(`${__dirname}/not-a-file.txt`, function(err) {
-        expect(err).to.be.an('error');
-        done();
+// below is code to be deleted for final submission
+// describe('File Reader Module', function() {
+//   describe('with an improper file path', function() {
+//     it('should return an error', function(done) {
+//       fileReader(`${__dirname}/not-a-file.txt`, function(err) {
+//         expect(err).to.be.an('error');
+//         done();
+//       });
+//     });
+//   });
+
+  // my own testing block
+  describe('test reading three files from data folder', function() {
+    describe('test reading file one', function () {
+      it('should return the first 8 bytes in hex', function(done) {
+        fileReader(`${__dirname}/../data/one.txt`, function(err, data) {
+          expect(err).to.equal(null);
+          expect(data).to.be.a('string');
+          expect(data).to.contain('1');
+          done();
+        });
       });
     });
+
+    describe('test reading file two', function() {
+      it('should return the first 8 bytes in hex', function(done) {
+        fileReader(`${__dirname}/../data/two.txt`, function(err, data) {
+          expect(err).to.be.equal(null);
+          expect(data).to.be.a('string');
+          expect(data).to.contain('2');
+          done();
+        });
+      });
+    });
+
+    describe('test reading file two', function() {
+      it('should return the first 8 byes in hex', function(done) {
+        fileReader(`${__dirname}/../data/three.txt`, function(err,data) {
+          expect(err).to.be.equal(null);
+          expect(data).to.be.a('string');
+          expect(data).to.contain('3');
+          done();
+        });
+      });
+    });
+
   });
 
-  describe('with a proper file path', function() {
-    it('should return the contents of the file', function(done) {
-      fileReader(`${__dirname}/../data/data.txt`, function(err, data) {
-        expect(err).to.equal(null);
-        expect(data).to.be.a('string');
-        expect(data).to.equal('sample data to be read from the read file demo\n');
-        done();
-      });
-    });
-  });
+// below is code to be deleted for final submission
+  // describe('testing reading file one', function() {
+  //   it('should return the contents of the file', function(done) {
+  //     fileReader(`${__dirname}/../data/one.txt`, function(err, data) {
+  //       expect(err).to.equal(null);
+  //       expect(data).to.be.a('string');
+  //       expect(data).to.contain('1');
+        // expect(data).to.equal('sample data to be read from the read file demo\n');
+  //       done();
+  //     });
+  //   });
+  // });
+
+  // the below was copied from example code
+  // describe('with a proper file path', function() {
+  //   it('should return the contents of the file', function(done) {
+  //     fileReader(`${__dirname}/../data/data.txt`, function(err, data) {
+  //       expect(err).to.equal(null);
+  //       expect(data).to.be.a('string');
+        // expect(data).to.equal('sample data to be read from the read file demo\n');
+  //       done();
+  //     });
+  //   });
+  // });
 });
