82676ee7e1f74f5850f4da70465dcfe1684c3619
Shawn Farrow <shaallfar@gmail.com>
2016-12-09 09:51:05 -0800

    updated mine to reflect Brians in class example

--- lab-shawn/lib/files.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-module.exports.readFileContent =  function(data) {
-    console.log('file:', data.toString('hex'));
-}
--- lab-shawn/test/file-test.js
+++ lab-shawn/test/file-test.js
@@ -1,16 +1,33 @@
 'use strict';
 
-const files = require('../index.js');
 const expect = require('chai').expect;
+const fileReader = require('../lib/file-reader.js');
 
 describe('File Module', function(){
-  describe('#readFileContent', function(){
-    it('should gurantee the files are read in the proper order', function(){
-        var result = files.readFileContent(null,);
-
+  describe('with bad filepath', function(){
+    it('should return error', function(){
+        fileReader([`${__dirname}/dont-exist.txt`], function(){
+          expect(err).to.equal(true);
+        });
     });
-    it('should check for improper paths', function(){
+  });
+  describe('with good filepath', function(){
+    before((done) => {
+      this.paths = [`${__dirname}/../data/one.txt`,
+                    `${__dirname}/../data/two.txt`,
+                    `${__dirname}/../data/three.txt`];
+      done();
+    });
 
-    })
+  it('should have correct order of strings', (done) => {
+    var expectedResult = ['6669727374206669','7365636f6e642066','7468697264206669'];
+    fileReader(this.paths, function(err,data){
+      expect(err).to.equal(null);
+      expect(data[0]).to.equal(expectedResult[0]);
+      expect(data[1]).to.equal(expectedResult[1]);
+      expect(data[2]).to.equal(expectedResult[2]);
+      done();
+      });
+    });
   });
 });
